2dd4ded767102c362837543dab9a55290be428f5
bkyalo <56555728+bkyalo@users.noreply.github.com>
2025-04-16 09:35:45 +0300

    Update on course unit controller

--- app/Models/Programme.php
+++ app/Models/Programme.php
@@ -13,7 +13,7 @@ class Programme extends Model
 
     public function courseUnitMappings()
     {
-        return $this->hasMany(\App\Models\CourseUnitProgrammeMapping::class);
+        return $this->hasMany(CourseUnitProgrammeMapping::class);
     }
 
     public function courseUnits()
@@ -26,5 +26,31 @@ class Programme extends Model
     public function school()
     {
         return $this->belongsTo(School::class);
+    }
+
+    public function lecturers()
+    {
+        return $this->hasManyThrough(
+            Lecturer::class, // Related model
+            CourseUnitProgrammeMapping::class, // The through model
+            'programme_id', // Foreign key on the 'course_unit_programme_mappings' table (points to the related 'programme')
+            'id', // Foreign key on the 'lecturers' table (points to 'lecturer_id')
+            'id', // Local key on the 'programmes' table
+            'lecturer_id' // Local key on the 'course_unit_programme_mappings' table (points to 'lecturer_id')
+        );
+    }
+
+    // Get lecturers teaching this specific CourseUnit
+    public function lecturersForCourseUnit($courseUnitId)
+    {
+        return $this->hasManyThrough(
+            Lecturer::class, // Related model (lecturer)
+            CourseUnitProgrammeMapping::class, // The "through" model
+            'programme_id', // Foreign key on the `course_unit_programme_mappings` table
+            'id', // Foreign key on the `lecturers` table (points to `lecturer_id`)
+            'id', // Local key on the `programmes` table
+            'lecturer_id' // Local key on the `course_unit_programme_mappings` table (points to `lecturer_id`)
+        )
+            ->where('course_unit_programme_mappings.course_unit_id', $courseUnitId); // Filter by specific course unit
     }
 }
