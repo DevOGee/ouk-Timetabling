187bc3ebd8562f00f93f971baa2af553751d3645
Ben TITO <benkyalo075@gmail.com>
2025-07-11 16:50:56 +0300

    Fix missing School model import in AcademicSessionController

--- app/Http/Controllers/Admin/AcademicSessionController.php
+++ app/Http/Controllers/Admin/AcademicSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\AcademicSession;
+use App\Models\School;
 use App\Models\Programme;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
